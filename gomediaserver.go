b9a5d6b0879237b6a8e9ef156716497bd1a1ed1c
Nixes <callum@see-bry.com>
2016-02-13 18:31:21 +1100

    added config file loading

--- gomediaserver.go
+++ gomediaserver.go
@@ -6,16 +6,51 @@ import (
   "net/http"
   "strings"
   "io/ioutil"
+  "encoding/json"
   "fmt"
 )
 
+type Settings struct {
+    FileFolder string
+    VideoFolder string
+    MusicFolder string
+}
+
 type Page struct {
     Title string
-    Body  []byte
+    CurrentSection string // store name of the currently activated section of the website
+}
+
+func LoadConfig () Settings {
+  content, err := ioutil.ReadFile("config.json")
+  if err!=nil{
+      fmt.Print("Error:",err)
+      var TempConfig Settings = Settings{ FileFolder:"./",VideoFolder:"./",MusicFolder:"./" }
+      content,err :=json.Marshal(&TempConfig)
+      if err!=nil{
+          fmt.Print("Error:",err)
+          fmt.Print("Failed to serialise config file")
+      }
+      err = ioutil.WriteFile("config.json",content,0666)
+      if err!=nil{
+          fmt.Print("Error:",err)
+          fmt.Print("Failed to write config file")
+      }
+  }
+  var settings Settings
+  err=json.Unmarshal(content, &settings)
+  if err!=nil{
+      fmt.Print("Error:",err)
+  }
+  return settings
+}
+
+func FolderScan (path string) {
+
 }
 
 func ImageBrowseHandler (w http.ResponseWriter, r *http.Request) {
-  
+  fmt.Printf("Image page requested.\n")
 }
 
 func FolderBrowseHandler (w http.ResponseWriter, r *http.Request) {
@@ -24,7 +59,7 @@ func FolderBrowseHandler (w http.ResponseWriter, r *http.Request) {
   real_path := strings.TrimPrefix(full_path, "files/");
 
   //fmt.Fprintf(w, real_path)
-  contents_array, err := ioutil.ReadDir("./"+real_path);
+  contents_array, err := ioutil.ReadDir(config.FileFolder + real_path);
   if err != nil {
       //panic(err)
       fmt.Fprintf(w, err.Error())
@@ -37,15 +72,18 @@ func FolderBrowseHandler (w http.ResponseWriter, r *http.Request) {
 func HomeHandler (w http.ResponseWriter, r *http.Request) {
   fmt.Printf("Home page requested.\n")
   //fmt.Fprintf(w, "Hi there, I love %s!")
-
   t := template.Must(template.ParseFiles("templates/main.html","templates/header.html","templates/footer.html") )  // Parse template file.
   t.Execute(w, nil)
 }
 
+var config Settings = LoadConfig();
+
 func main() {
+
   http.HandleFunc("/static/", func(w http.ResponseWriter, r *http.Request) {
       http.ServeFile(w, r, r.URL.Path[1:])
   })
+  http.HandleFunc("/images", ImageBrowseHandler)
   http.HandleFunc("/files", func(w http.ResponseWriter, r *http.Request) { http.Redirect(w, r, "/files/", 301)} )
   http.HandleFunc("/files/", FolderBrowseHandler)
   http.HandleFunc("/", HomeHandler)
