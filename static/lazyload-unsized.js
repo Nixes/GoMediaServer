03e35db2d1d7d62efee6767f30f9d80f8ae206c7
Nixes <callum@see-bry.com>
2016-03-18 17:35:26 +1100

    working on demand image loading

--- static/lazyload-unsized.js
+++ static/lazyload-unsized.js
@@ -1,17 +1,58 @@
-!function(window){
-
-  function loadImage (el, fn) {
-    var img = new Image()
-      , src = el.getAttribute('data-src');
+function loadImage (el) {
+    var img = new Image(), src = el.getAttribute('data-src');
     img.onload = function() {
+      console.log("Image finished Loading");
       if (!! el.parent)
         el.parent.replaceChild(img, el)
       else
         el.src = src;
 
-      fn? fn() : null;
+      finishLoadingImage(el)
+      //fn? fn() : null;
     }
     img.src = src;
-  }
+}
 
-}(this);
+function loadImages() {
+  console.log("Loading the next bunch")
+  $( ".dimmer" ).addClass( "active" );
+
+  var images = $( ".unloaded" );
+  for (i = 0; i < 10 ; i++) {
+    loading_images += 1;
+    loadImage(images[i])
+  }
+}
+
+function finishLoadingImage (el) {
+  //console.log(el);
+  el.classList.remove("unloaded");
+  loading_images -= 1;
+  if (loading_images < 1) {
+    finishLoadingImages();
+  }
+}
+
+function finishLoadingImages () {
+  $( ".dimmer" ).removeClass( "active" ); // removes loading placeholder
+  console.log("Finished Loading Images");
+}
+
+function checkScroll () {
+	var body = document.body, html = document.documentElement;
+	var documentHeight = Math.max( body.scrollHeight, body.offsetHeight,
+                       html.clientHeight, html.scrollHeight, html.offsetHeight );
+
+	console.log( "window.pageYOffset: " + window.pageYOffset + " documentHeight " + documentHeight + " window.innerHeight: " + window.innerHeight );
+	if ( window.pageYOffset + window.innerHeight > (documentHeight - 100) ) {
+    loadImages();
+	}
+}
+
+var loading_images = 0;
+
+function init () {
+   loadImages();
+   window.onscroll = checkScroll; // only check after the page has loaded
+}
+window.onload = init;
